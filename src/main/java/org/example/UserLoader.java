668bcb6466f60d215d8df2c3551815a91101694e
IrinaPleshakova <irynapleshakova@gmail.com>
2024-08-06 09:25:38 +0200

    Update UserLoader.java, UserProcessor.java and pom.xml
    1. Implement user filtering and sorting in UserProcessor.java
      - Added imports for Comparator and Java streams.
      - Implemented filtering of users over 25 years old and with specific email domains.
      - Sorted users by name in ascending order using streams and comparators.
    2. Add Gson dependency to pom.xml
      - Added Gson dependency to the Maven pom.xml file for JSON deserialization support.
    3. Update UserLoader.java to use Gson for JSON deserialization
      - Added necessary imports for Gson, File handling, and reflection.
      - Implemented JSON deserialization logic using Gson to read from a file and convert JSON content into a list of User objects.
      - Added error handling for JSON parsing exceptions and file not found exceptions.

--- src/main/java/org/example/UserLoader.java
+++ src/main/java/org/example/UserLoader.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
 import org.example.dto.User;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class UserLoader {
@@ -12,7 +17,22 @@ public class UserLoader {
         // з JSON файлу який знаходиться за відносним шляхом filepath
         // з допомогою вбудованих засобів бібліотеки https://mvnrepository.com/artifact/com.google.code.gson/gson/2.10.1
         // десеріалізує контент JSON файлу у список з об*єктів типу User і повертає список
+        Gson gson = new Gson();
+        Type userListType = new TypeToken<List<User>>() {
+        }.getType();
 
-        return null;
+        try (FileReader reader = new FileReader(filePath)) {
+            List<User> users = gson.fromJson(reader, userListType);
+
+            if (users == null) {
+                throw new JsonParseException("JSON content is empty or invalid");
+            }
+
+            return users;
+        } catch (JsonParseException e) {
+
+            System.err.println("Failed to parse JSON: " + e.getMessage());
+            throw e;
+        }
     }
 }
